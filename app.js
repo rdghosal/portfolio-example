613b453630a6df96233a97c97d6fda9a5707e720
Rahul Ghosal <rdghosal@gmail.com>
2023-03-28 07:14:52 -0400

    feat: persist scheme settings, refactors

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
+const DEFAULT_SCHEME = 'dark';
+const SCHEME_KEY = 'scheme';
+const SWITCH_NAME = 'mode-switch'
+
+const ColorScheme = {
+    DARK: 'dark',
+    LIGHT: 'light',
+};
+
 function toggleScheme(checked) {
     if (!checked) {
         document.querySelectorAll('*').forEach((element) => {
-            if (!element.classList.contains('mode-switch')) {
-                element.classList.add('light');
+            if (!element.classList.contains(SWITCH_NAME)) {
+                element.classList.add(ColorScheme.LIGHT);
             }
         });
+        window.localStorage.setItem(SCHEME_KEY, ColorScheme.LIGHT);
     } else {
         document.querySelectorAll('*').forEach((element) => {
             if (
-                !element.classList.contains('mode-switch') 
+                !element.classList.contains(SWITCH_NAME) 
                 &&
-                element.classList.contains('light')
+                element.classList.contains(ColorScheme.LIGHT)
             ) {
-                element.classList.remove('light');
+                element.classList.remove(ColorScheme.LIGHT);
             }
         });
+        window.localStorage.setItem(SCHEME_KEY, ColorScheme.DARK);
     }
 };
 
-const schemeToggler = document.getElementById('mode-switch');
+const currScheme = window.localStorage.getItem(SCHEME_KEY) || DEFAULT_SCHEME;
+const schemeToggler = document.getElementById(SWITCH_NAME);
+const checked = currScheme === DEFAULT_SCHEME
+
 schemeToggler.addEventListener('change', () => toggleScheme(schemeToggler.checked));
+schemeToggler.checked = checked;
+toggleScheme(checked);
 
